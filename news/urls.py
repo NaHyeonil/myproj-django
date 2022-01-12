d2d1009cd1bfa67e8d2229326599e22c315d8919
Hyeonil Na <sjrnfl528@gmail.com>
2022-01-12 16:23:14 +0900

    news.Article REST API 생성

--- news/urls.py
+++ news/urls.py
@@ -1,3 +1,14 @@
-app_name = "news"
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = []
+from news.views import ArticleViewSet
+
+app_name = 'news'
+
+
+router = DefaultRouter()
+router.register("articles", ArticleViewSet)
+
+urlpatterns = [
+    path("api/", include(router.urls)),
+]
